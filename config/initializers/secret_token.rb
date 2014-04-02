a1f0a9d486eff93f5f792ecec4f397d156066f42
Danny Whalen <daniel.r.whalen@gmail.com>
2014-04-02 10:04:22 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+WhatsHapp::Application.config.secret_key_base = 'a3b9f9fd59c244ba544313157e5aaa0ff043fba6a6e01b4bd09b8e13a0400b2e03506080817ec356f7e3cfe8bda67d6893310ee2eb5c85eb8ca7312b7c671395'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+WhatsHapp::Application.config.session_store :cookie_store, key: '_whats_happ_session'
