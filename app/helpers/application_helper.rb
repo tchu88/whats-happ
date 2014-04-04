35f2781380ac8b916401fb294a8290f4ecd1e780
Danny Whalen <daniel.r.whalen@gmail.com>
2014-04-03 19:09:30 -0700

    Bootstrap sass integration

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  FLASH_CLASSES = {
+    alert: "danger",
+    error: "danger",
+    notice: "success",
+  }.with_indifferent_access.freeze
+
+  def flash_class(name)
+    FLASH_CLASSES[name] || name
+  end
+
+  def navbar_list_item(text, options={})
+    return if options.delete(:hide)
+    render 'navbar_list_item', text: text, path: yield, options: options
+  end
 end
