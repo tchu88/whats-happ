ba97942aea50844ca746147661bd88c19d5190f4
Danny Whalen <daniel.r.whalen@gmail.com>
2014-04-04 00:17:06 -0700

    Refactor geocoding and updating map

--- app/assets/javascripts/geo.js
+++ app/assets/javascripts/geo.js
@@ -1,32 +1,32 @@
-function setupMap(){
-  var map = L.map('subscription_map').setView([37.7756648, -122.4136613], 12);
-  L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png').addTo(map);
-}
+$(function (){
+  if (!$('body').hasClass('subscriptions-new')) return;
 
-function setupGeocoding(){
-  function isBlank(str) { return (!str || /^\s*$/.test(str)); }
+  var initialCoords = [37.7756648, -122.4136613];
+  var map = L.map('subscription_map').setView(initialCoords, 12);
+  L.tileLayer('http://{s}.tile.stamen.com/toner/{z}/{x}/{y}.png').addTo(map);
+
+  function isBlank(str) {
+    return (!str || /^\s*$/.test(str));
+  }
   function setLatLon(coords) {
     $('#subscription_longitude').val(coords.lon);
     $('#subscription_latitude').val(coords.lat);
+    return coords;
   }
-  function geocode() {
+  function centerMap(coords){
+    if (coords.lat && coords.lon) map.setView(new L.LatLng(coords.lat, coords.lon));
+    return coords;
+  }
+  function geocodeURL(address){
+    return "http://nominatim.openstreetmap.org/search?q="+
+           encodeURIComponent(address)+
+           "&format=json&limit=1&addressdetails=1";
+  }
+  function updateLocation() {
     var address = $(this).val();
-
-    // clear hidden fields and return if address is blank
-    if (isBlank(address)) {
-      setLatLon({lat: null, lon: null});
-      return;
-    }
-
-    var url = "http://nominatim.openstreetmap.org/search?q="+
-              encodeURIComponent(address)+
-              "&format=json&limit=1&addressdetails=1";
-
-    $.getJSON(url, _.compose(setLatLon, _.first));
+    if (isBlank(address)) return setLatLon({lat: null, lon: null});
+    $.getJSON(geocodeURL(address), _.compose(setLatLon, centerMap, _.first));
   }
 
-  $('#subscription_address').change(geocode);
-}
-
-$(setupMap);
-$(setupGeocoding);
+  $('#subscription_address').change(updateLocation);
+});
