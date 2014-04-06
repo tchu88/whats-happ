522fdb7cb435b3aad8d381e80545007877955494
Danny Whalen <daniel.r.whalen@gmail.com>
2014-04-06 10:58:12 -0700

    Extract stub sms method for specs

--- spec/services/sms_notification_spec.rb
+++ spec/services/sms_notification_spec.rb
@@ -5,11 +5,19 @@ describe SMSNotification do
   let(:phone_number) { '+6021239876' }
   let(:body) { 'Summer sundays, sailing swiftly south' }
 
-  it 'sends an sms notification' do
-    stub_request(:post, "https://fakeaccountsid:fakeauthtoken@api.twilio.com/2010-04-01/Accounts/fakeaccountsid/Messages.json").
-      with(body: {"Body"=>body, "From"=>SMSNotification::FROM_NUMBER, "To"=>phone_number}).
+  def stub_sms(phone, body)
+    url = "https://%s:%s@api.twilio.com/2010-04-01/Accounts/%s/Messages.json" % [
+      SMSNotification::ACCOUNT_SID,
+      SMSNotification::AUTH_TOKEN,
+      SMSNotification::ACCOUNT_SID
+    ]
+    stub_request(:post, url).
+      with(body: {"Body"=>body, "From"=>SMSNotification::FROM_NUMBER, "To"=>phone}).
       to_return(status: 200, body: JSON.generate({}))
+  end
 
+  it 'sends an sms notification' do
+    stub_sms(phone_number, body)
     notification.call
   end
 end
