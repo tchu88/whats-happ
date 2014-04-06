46ecac57da8908656960df64f1b8de4413bda86b
Danny Whalen <daniel.r.whalen@gmail.com>
2014-04-05 21:48:09 -0700

    Refactor notifier

--- app/services/notifier.rb
+++ app/services/notifier.rb
@@ -1,20 +1,30 @@
-class Notifier < Struct.new(:event)
+class Notifier
+  include Enumerable
+
   def self.call(events)
     events.each do |event|
       new(event).call
     end
   end
 
+  def initialize(event)
+    @event = event
+  end
+
+  def each(&block)
+    subscriptions.find_each(&block)
+  end
+
   def call
-    subscriptions.each do |subscription|
-      send_message(subscription.phone, event.message)
+    each do |subscription|
+      send_message(subscription.phone, @event.message)
     end
   end
 
   def send_message(phone, body)
   end
 
-  def subscriptions
-    Subscription.contains(latitude: event.latitude, longitude: event.longitude)
+  def subscriptions(&block)
+    Subscription.contains(latitude: @event.latitude, longitude: @event.longitude)
   end
 end
